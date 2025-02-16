40b1faa7d5c60b43819b13f5da0362e07d9df47a
Remp100 <ionutalexandruculea@gmail.com>
2025-02-16 20:42:23 +0200

    Fix Server

--- server/app.js
+++ server/app.js
@@ -107,8 +107,9 @@ const storage = new CloudinaryStorage({
 const upload = multer({ storage: storage });
 
 // Start the server
-app.listen(3002, () => {
-  console.log("Server is running on port 3002");
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // Connect to MongoDB
